2c3109e84d91ca5f33701e6488d8da0e85eb997a
yogaputra0107 <yogaputra0107@gmail.com>
2025-03-27 20:19:06 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "@/components/layout/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shadcn - Landing template",
-  description: "Landing template from Shadcn",
+  title: "Genfiit",
+  description: "Landing Genfiit",
 };
 
 export default function RootLayout({
